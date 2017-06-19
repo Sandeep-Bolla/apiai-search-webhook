d14493fed568e42e5fbc12a70b31c6dedc562e66
Hari Baskar <haribaskar98@gmail.com>
2017-06-19 18:41:45 +0000

    initial commit

---
+++ app.py
@@ -0,0 +1,117 @@
+
+
+#!/usr/bin/env python
+
+from __future__ import print_function
+from future.standard_library import install_aliases
+install_aliases()
+
+from urllib.parse import urlparse, urlencode
+from urllib.request import urlopen, Request
+from urllib.error import HTTPError
+
+import json
+import os
+
+from flask import Flask
+from flask import request
+from flask import make_response
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import urllib.request
+import re
+
+
+# Flask app should start in global layout
+app = Flask(__name__)
+
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    req = request.get_json(silent=True, force=True)
+
+    #print("Request:")
+    #print(json.dumps(req, indent=4))
+
+    res = processRequest(req)
+
+    res = json.dumps(res, indent=4)
+    print(res)
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    return r
+
+
+def processRequest(req):
+    if req.get("result").get("action") != "search":
+        return {}
+    result = req.get("result")
+    parameters = result.get("parameters")
+    city = parameters.get("city")
+    print(city)
+    if city is None:
+        return None
+    result=search(city)
+    
+    #data = json.loads(req)
+    res = makeWebhookResult(result)
+    return res
+
+
+
+
+
+def makeWebhookResult(data):
+    
+    #print("Response:")
+    #print(speech)
+
+    return {
+        "speech": str(data),
+        "displayText": "",
+        # "data": data,
+        # "contextOut": [],
+        "source": "apiai-weather-webhook-sample"
+    }
+
+def search(query):
+    query=query.strip().split()
+    query="+".join(query)
+
+    html = "https://www.google.co.in/search?site=&source=hp&q="+query+"&gws_rd=ssl"
+    req = urllib.request.Request(html, headers={'User-Agent': 'Mozilla/5.0'})
+
+    soup = BeautifulSoup(urlopen(req).read(),"html.parser")
+
+    #Regex
+    reg=re.compile(".*&sa=")
+    search =[]
+    links = []
+    title = []
+    body  = []
+    #parsing title
+    for item in soup.find_all(attrs={'class' : 'r'}):
+            title.append(item.a.contents)
+    
+    #parsing body
+    for item in soup.find_all(attrs={'class' : 'st'}):
+            body.append(item.contents)
+
+    #Parsing web urls
+    for item in soup.find_all('h3', attrs={'class' : 'r'}):
+            line = (reg.match(item.a['href'][7:]).group())
+            links.append(line[:-4])
+    print(title)
+    search.append(title)
+    search.append(body)
+    search.append(links)
+
+    return search
+
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 5004))
+
+    print("Starting app on port %d" % port)
+
+    app.run(debug=False, port=port, host='0.0.0.0')
+
